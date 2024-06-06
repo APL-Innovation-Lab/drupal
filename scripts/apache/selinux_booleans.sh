8e37d3abc3da8d430409e98ac66d8bc0f4a88123
brycebenton <bryce.benton@austintexas.gov>
2024-06-06 15:20:19 -0500

    selinux booleans

---
+++ scripts/apache/selinux_booleans.sh
@@ -0,0 +1,15 @@
+# View current settings
+sudo getsebool httpd_enable_homedirs
+sudo getsebool httpd_can_network_connect
+sudo getsebool httpd_can_network_relay
+sudo getsebool httpd_can_sendmail
+sudo getsebool httpd_read_user_content
+sudo getsebool httpd_unified
+
+# Set them to the desired state if needed
+sudo setsebool -P httpd_enable_homedirs on
+sudo setsebool -P httpd_can_network_connect on
+sudo setsebool -P httpd_can_network_relay on
+sudo setsebool -P httpd_can_sendmail on
+sudo setsebool -P httpd_read_user_content on
+sudo setsebool -P httpd_unified on
