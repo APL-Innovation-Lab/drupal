d4b521918e944c45b3efbe07fe51c1b970208d39
brycebenton <bryce.benton@austintexas.gov>
2024-06-11 12:33:55 -0500

    nfs selinux boolean

--- scripts/apache/selinux_booleans.sh
+++ scripts/apache/selinux_booleans.sh
@@ -5,6 +5,7 @@ sudo getsebool httpd_can_network_relay
 sudo getsebool httpd_can_sendmail
 sudo getsebool httpd_read_user_content
 sudo getsebool httpd_unified
+sudo getsebool httpd_use_nfs on
 
 # Set them to the desired state if needed
 sudo setsebool -P httpd_enable_homedirs on
@@ -13,3 +14,4 @@ sudo setsebool -P httpd_can_network_relay on
 sudo setsebool -P httpd_can_sendmail on
 sudo setsebool -P httpd_read_user_content on
 sudo setsebool -P httpd_unified on
+sudo setsebool -P httpd_use_nfs on
